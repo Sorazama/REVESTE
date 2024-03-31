874e51df985d6f16ad3a767083559cd68d9072a3
Ka1quee <kaiqueoliveira375@gmail.com>
2024-03-31 18:37:37 -0300

    Arrumando animação dos selos

--- JavaScript/Breshop.js
+++ JavaScript/Breshop.js
@@ -1,21 +1,23 @@
+var selos = document.querySelectorAll('.selo-beneficiente');
+var paragrafos = document.querySelectorAll('.detalhes');
 
-var paragrafo = document.querySelector('.detalhes')
-var selo = document.querySelector('.selo-beneficiente')
+// Itera sobre cada selo e adiciona os ouvintes de eventos
+selos.forEach(function(selo, index) {
+  selo.addEventListener('mouseover', function() {
+    mostraDetalhes(paragrafos[index], selo);
+  });
+  selo.addEventListener('mouseleave', function() {
+    esconde(paragrafos[index], selo);
+  });
+});
 
-// adicionando evento de click
-
-selo.addEventListener('mouseover', mostraDetalhes)
-selo.addEventListener('mouseleave', esconde)
-
-
-
-// função que apresentará detalhes do card
-function mostraDetalhes() {
-  selo.classList.add('selo-detalhes')
-  paragrafo.classList.add('texto-visivel')
+// Função que apresentará detalhes do card
+function mostraDetalhes(paragrafo, selo) {
+  paragrafo.classList.add('texto-visivel');
+  selo.classList.add('selo-detalhes');
 }
 
-function esconde() {
-  selo.classList.remove('selo-detalhes')
-  paragrafo.classList.remove('texto-visivel')
+function esconde(paragrafo, selo) {
+  paragrafo.classList.remove('texto-visivel');
+  selo.classList.remove('selo-detalhes');
 }
