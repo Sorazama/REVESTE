f85f5b30cf7c664c1f01ddc40a830cd6acc99b00
sorazama <gabrielmaciels@hotmail.com>
2024-04-05 20:02:55 -0300

    atualização do cesar

---
+++ JavaScript/header.js
@@ -0,0 +1,11 @@
+
+window.addEventListener("scroll", function(){
+    let nav = document.querySelector('.cabecalho-inferior')
+    nav.classList.toggle('rolagem-nav',window.scrollY > 160)
+})
+
+window.addEventListener("scroll", function(){
+    let nav_link = document.querySelector('#nav-link')
+    nav_link.classList.toggle('nav-list',window.scrollY > 200)
+})
+
